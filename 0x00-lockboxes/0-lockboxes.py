5dd421a59fa00074f27415d22ac99b70c0ccc499
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-07-29 03:48:56 +0000

    [Feature] method that determines if all the boxes can be opened.

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,7 +4,7 @@
 
 
 def canUnlockAll(boxes):
-    """ 
+    """
     method that determines if all the boxes can be opened
     boxes is a list of lists
     A key with the same number as a box opens that box
@@ -13,8 +13,8 @@ def canUnlockAll(boxes):
     Return True if all boxes can be opened, else return False
     """
 
-
     unlocked = [0]
+
     for i in unlocked:
         for j in boxes[i]:
             if j < len(boxes):
@@ -22,7 +22,6 @@ def canUnlockAll(boxes):
                     pass
                 else:
                     unlocked.append(j)
-
 
     if len(unlocked) == len(boxes):
         return True
