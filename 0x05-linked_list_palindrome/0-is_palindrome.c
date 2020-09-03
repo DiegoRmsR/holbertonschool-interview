12386e75b686a1551cda3364afde11935b95625c
DiegoRmsR <diegoramos.mt@gmail.com>
2020-09-02 23:49:04 -0500

    [Feature] Linked list palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - Verify if a linked list is palindrome
+ * @head: (listint_t **) Head of the node
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	int length_all = length_linked(*head);
+	int length_half = (length_all / 2), step = 0;
+	listint_t *left = *head, *right = NULL;
+	size_t j = 0, i = 1;
+
+	if (!length_all)
+		return (1);
+
+	for (; step < length_half; ++step)
+	{
+		right = *head;
+
+		j = (length_all - step);
+		/* Get the node of the right */
+		for (i = 1; (j < i) && (right->next); ++j)
+			right = right->next;
+
+		if (left->n != right->n)
+			return (0);
+
+		left = left->next;
+	}
+
+	return (1);
+}
+
+/**
+ * length_linked - Look amount of nodes
+ * @head: Copy of the Head node
+ *
+ * Return: Length of the linked list
+ */
+int length_linked(listint_t *head)
+{
+	int i = 0;
+
+	for (; head != NULL; ++i, (head = head->next))
+		;
+
+	return (i);
+}
