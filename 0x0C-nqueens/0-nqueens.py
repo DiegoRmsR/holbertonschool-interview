760209a31c4504a5f5d263d73b2d8f89637639af
DiegoRmsR <diegoramos.mt@gmail.com>
2020-11-04 21:26:34 -0500

    [Feature] 0. N queens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -15,11 +15,7 @@ class NQueen:
     def place(self, k, i):
         """ Checks if k Queen can be placed
         """
-
-        # j checks from 1 to k - 1 (Up to previous queen)
         for j in range(1, k):
-            # There is already a queen in column
-            # or a queen in same diagonal
             if self.x[j] == i or \
                abs(self.x[j] - i) == abs(j - k):
                 return 0
@@ -27,24 +23,17 @@ class NQueen:
 
     def nQueen(self, k):
         """ N QUEEN """
-        # i goes from column 1 to column n (1st column is 1st index)
         for i in range(1, self.n + 1):
             if self.place(k, i):
-                # Queen can be placed in i column
                 self.x[k] = i
                 if k == self.n:
-                    # Placed all 4 Queens (A solution was found)
                     solution = []
                     for i in range(1, self.n + 1):
                         solution.append([i - 1, self.x[i] - 1])
                     self.res.append(solution)
                 else:
-                    # Need to place more Queens
                     self.nQueen(k + 1)
         return self.res
-
-
-# Main
 
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
@@ -62,8 +51,8 @@ if N < 4:
     print("N must be at least 4")
     sys.exit(1)
 
-nqueen = NQueen(N)
-result = nqueen.nQueen(1)
+queen = NQueen(N)
+result = queen.nQueen(1)
 
 for i in result:
     print(i)
